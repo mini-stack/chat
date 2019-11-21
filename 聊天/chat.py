7b03f78a22541349761122eda9304f6073a6be6e
mini-stack <17610786121@163.com>
2019-11-21 11:08:50 +0800

    Add files via upload

---
+++ 聊天/chat.py
@@ -0,0 +1,17 @@
+from ministack import *            # 导入创建窗口工具
+creat_dialog()                            # 创建窗口
+ck = UI_Name()                           # 给窗口起个名字叫ck
+ck.addTitle("聊天")                      # 给ck窗口添加标题
+ck.setBg("img/bg.png")                 # 设置窗口的背景图片
+ck.left_img("img/小栈_1.png")   #  设置左侧头像
+ck.right_img("img/小栈_2.png")  #   设置右侧头像
+ck.addText()                             # 给ck窗口添加输入框
+ck.end_dialog()                         # 给ck窗口添加关闭功能                         
+
+
+
+
+
+
+
+
